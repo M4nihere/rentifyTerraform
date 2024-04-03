d20667ed8e755939a5dda8f2c5159daf9dd2a658
root <root@rentifyHostTerraform.local>
2024-04-03 11:40:59 +0530

    Added Private and Public Subnets within Beanstalk-VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -5,3 +5,44 @@ resource "aws_vpc" "main" {
    Name = "Beanstalk-VPC"
  }
 }
+
+
+resource "aws_subnet" "Beanstalk-Public-Subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "Beanstalk-Public-Subnet1"
+  }
+}
+
+resource "aws_subnet" "Beanstalk-Private-Subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "Beanstalk-Private-Subnet1"
+  }
+}
+
+resource "aws_subnet" "Beanstalk-Public-Subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-southeast-2b"
+
+  tags = {
+    Name = "Beanstalk-Public-Subnet2"
+  }
+}
+
+resource "aws_subnet" "Beanstalk-Private-Subnet2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "ap-southeast-2b"
+
+  tags = {
+    Name = "Beanstalk-Private-Subnet2"
+  }
+}
